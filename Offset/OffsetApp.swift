15ce7ccdf1a6dc6df4d0658cac8d2fd673094399
sivansundar <sivan.sundar@gmail.com>
2026-04-13 19:18:18 +0530

    Initial Commit

---
+++ Offset/OffsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  OffsetApp.swift
+//  Offset
+//
+//  Created by Sivan on 13/04/26.
+//
+
+import SwiftUI
+
+@main
+struct OffsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
